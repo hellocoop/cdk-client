e1eb1203a536a94fb87078d27877b1e12b546e91
dickhardt <dick.hardt@hello.coop>
2024-03-01 17:33:35 -0800

    rename

--- cdk-test/bin/cdk.ts
+++ cdk-test/bin/cdk.ts
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { ClientTestStack } from '../lib/client-test-stack';
+import { ClientSampleStack } from '../lib/client-sample-stack';
 
+const account = process.env.CDK_DEFAULT_ACCOUNT
+const region = process.env.CDK_DEFAULT_REGION
+const env = { account, region }
 
 const app = new cdk.App();
-const test = new ClientTestStack(app, 'ClientTestStack');
+const test = new ClientSampleStack(app, 'HelloClientSampleStack', { env });
 
