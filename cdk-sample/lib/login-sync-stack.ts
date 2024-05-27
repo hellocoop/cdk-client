4188b86134870fac5d579186afb795b9870e1904
dickhardt <dick.hardt@hello.coop>
2024-05-27 10:05:17 -0700

    fix names

--- cdk-sample/lib/login-sync-stack.ts
+++ cdk-sample/lib/login-sync-stack.ts
@@ -1,4 +1,4 @@
-// A test trigger lambda
+// A sample login sync stack
 
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
@@ -6,16 +6,16 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 const LOGIN_SYNC_LAMBDA = 'loginSyncSample' 
 
-export class LoginSyncSample extends cdk.Stack {
+export class LoginSyncStack extends cdk.Stack {
   public readonly lambdaFunction: lambda.Function;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     this.lambdaFunction = new lambda.Function(this, LOGIN_SYNC_LAMBDA, {
         functionName: LOGIN_SYNC_LAMBDA,
-        runtime: lambda.Runtime.NODEJS_20_X, 
+        runtime: lambda.Runtime.NODEJS_18_X, 
         handler: 'index.handler',
-        code: lambda.Code.fromAsset('lambdas/sampple-login-sync'),      
+        code: lambda.Code.fromAsset('lambdas/sample-login-sync'),      
     });
 
     new cdk.CfnOutput(this, 'LoginSyncLambdaArn', { value: this.lambdaFunction.functionArn });
