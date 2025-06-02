0b5b3c6be83ce504e3efbfa84a8ea73ff89d31c0
rohanharikr <rohanharikumar80@gmail.com>
2025-06-02 18:43:08 +0100

    update lambda function runtime to NODEJS_22_X

--- src/client.ts
+++ src/client.ts
@@ -88,7 +88,7 @@ export class HelloClientConstruct extends Construct {
         const functionName = props.functionName || 'HelloClient'
         this.lambdaFunction = new lambda.Function(this, functionName, {
           functionName,
-          runtime: lambda.Runtime.NODEJS_20_X, // @DICK any reason why we don't use NODEJS_22_X here?
+          runtime: lambda.Runtime.NODEJS_22_X,
           handler: 'index.handler',
           code: lambda.Code.fromAsset(zipProtocolPath),
           environment,
